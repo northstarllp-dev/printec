72a522586803cd3d9eaf349197613a28063120fb
Akshay <aksahy.nitt04@gmail.com>
2026-06-13 15:58:25 +0530

    Initial commit

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -4,7 +4,7 @@ import { type NextRequest, NextResponse } from "next/server";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
-export const createClient = (request: NextRequest) => {
+export const updateSession = async (request: NextRequest) => {
   // Create an unmodified response
   let supabaseResponse = NextResponse.next({
     request: {
@@ -33,5 +33,9 @@ export const createClient = (request: NextRequest) => {
     },
   );
 
-  return supabaseResponse
+  // IMPORTANT: Avoid writing any logic between createServerClient and
+  // supabase.auth.getUser(). A simple mistake can write a secure cookie.
+  await supabase.auth.getUser();
+
+  return supabaseResponse;
 };
